7a0edf240d7dedd30b785be60d332142b7c1124a
p-t-a-p-1 <kb240225+1@gmail.com>
2020-05-22 13:34:59 +0900

    [update] ダブルアップゲームの勝敗判定実装、ゲーム機能は完成（得点計算などはまだ）

--- bonus.py
+++ bonus.py
@@ -3,6 +3,11 @@ import re
 
 class DoubleUp:
 
+    RANKS = (*"23456789", "10", *"JQKA")
+    VALUES = (range(2, 14 + 1))
+    # 表示マークとスコアを紐づける
+    RANK_TO_VALUES = dict(zip(RANKS, VALUES))
+
     def __init__(self, deck, player):
         self.deck = deck
         self.player = player
@@ -17,20 +22,39 @@ class DoubleUp:
         print(f"player's hands：{self.player.hands[0]}, *-*, *-*, *-*, *-*")
 
         # カードを1枚ずつ表示して番号を割り振る
+        check_hands = []
         for card_idx, card_val in enumerate(self.player.hands):
+            # print(f"{card_idx}：{card_val}")
+            # 連想配列に追加
+            check_card_set = str(card_val).split("-")
+            # ❤︎
+            card_mark = check_card_set[0]
+            # K
+            card_rank = check_card_set[1]
+            # 13
+            card_number = self.RANK_TO_VALUES[card_rank]
+            # チェック用の辞書に追加
+            check_hands.append({
+                "mark": card_mark,
+                "rank": card_rank,
+                "number": card_number
+            })
             # 1番目は選択できない
-            if card_idx == 0:
-                continue
-            print(f"{card_idx}：{card_val}")
+            if card_idx >= 1:
+                # 隠す
+                # print(f"{card_idx}：*-*")
+                print(f"{card_idx}：{card_val}")
 
         # カード番号を入力させる
         # TODO 4枚の裏向きカードの中から、表向きのカードの数字より強いものを1枚選ぶ
         card_select_msg = f"Enter a card number that is stronger than {self.player.hands[0]}："
         card_select_res = input(card_select_msg)
-        print(card_select_res)
 
         if re.compile(r'^[1-4]+$').match(card_select_res) is not None:
-            print(self.player.hands[int(card_select_res)])
+            if check_hands[int(card_select_res)]["number"] >= check_hands[0]["number"]:
+                print("win!")
+            else:
+                print("lose..")
         else:
             print("ダメです")
 
